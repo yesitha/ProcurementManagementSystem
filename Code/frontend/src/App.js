d47698cdd9e0331402465516500efe7d49ae58d5
NelakaGunawardhana <navindu3000@gmail.com>
2023-01-27 23:25:31 +0530

    Merge pull request #19 from yesitha/development-Yesitha
    Manage Actions Edited

--- Code/frontend/src/App.js
+++ Code/frontend/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
+<<<<<<< Updated upstream
+import ManageAuction from './pages/BidOpeningCommitee/ManageAuction/ManageAuction';
 
 import Dashboard from './pages/Dashboard/Dashboard';
 import NotificationPage from './pages/NotificationPage/NotificationPage';
 
 import SignUp from './pages/SignUp/SignUp';
+=======
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp'
+import SubProcurementPlan from './pages/Create SubProcurement Plan Division HOD/Create SubProcurementPlan'
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div>
+<<<<<<< Updated upstream
       
-    <NotificationPage/>
+    <ManageAuction/>
+=======
+     <SubProcurementPlan/>
+>>>>>>> Stashed changes
     </div>
   );
 }
