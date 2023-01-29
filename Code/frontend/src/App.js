9b16f94b6e9e5a45498069594e750b8d21454f57
Yesitha Sathsara <60166952+yesitha@users.noreply.github.com>
2023-01-29 11:33:28 +0530

    Master Procurement Bar Status Page Created

--- Code/frontend/src/App.js
+++ Code/frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <div>
 
-    <CreateSubProcurementPlan/>
+    <MasterProcurementPlanStatus/>
 
     </div>
   );
---
+++ Code/frontend/src/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Check from '@mui/icons-material/Check';
+import SettingsIcon from '@mui/icons-material/Settings';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import VideoLabelIcon from '@mui/icons-material/VideoLabel';
+import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
+import { Typography } from '@mui/material';
+
+
+
+
+
+
+const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
+  [`&.${stepConnectorClasses.alternativeLabel}`]: {
+    top: 22,
+  },
+  [`&.${stepConnectorClasses.active}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      backgroundImage:
+        'linear-gradient( 95deg, rgb(44,116,179) 0%,rgb(32,82,149) 50%,rgb(20,66,114) 100%)',
+        
+    },
+  },
+  [`&.${stepConnectorClasses.completed}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      backgroundImage:
+        'linear-gradient( 95deg, rgb(44,116,179) 0%,rgb(32,82,149) 50%,rgb(20,66,114) 100%)',
+       
+
+    },
+  },
+  [`& .${stepConnectorClasses.line}`]: {
+    height: 5,
+    border: 0,
+    backgroundColor:'#D9D9D9',
+    
+    borderRadius: 1,
+  },
+}));
+
+const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => ({
+  backgroundColor: '#D9D9D9',
+  zIndex: 1,
+  color: '#ffffff',
+  width: 50,
+  height: 50,
+  display: 'flex',
+  borderRadius: '50%',
+  justifyContent: 'center',
+  alignItems: 'center',
+  ...(ownerState.active && {
+    backgroundImage:
+      'linear-gradient( 136deg, rgb(44,116,179) 0%, rgb(32,82,149) 50%, rgb(20,66,114) 100%)',
+    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
+  }),
+  ...(ownerState.completed && {
+    backgroundImage:
+      'linear-gradient( 136deg, rgb(44,116,179) 0%, rgb(32,82,149) 50%, rgb(20,66,114) 100%)',
+  }),
+}));
+
+function ColorlibStepIcon(props) {
+  const { active, completed, className } = props;
+
+
+
+  return (
+    <ColorlibStepIconRoot ownerState={{ completed, active }} className={className}>
+      <Typography fontFamily='inter' fontWeight='600' fontSize='24px' >{String(props.icon)}</Typography>
+      
+    </ColorlibStepIconRoot>
+  );
+}
+
+ColorlibStepIcon.propTypes = {
+  /**
+   * Whether this step is active.
+   * @default false
+   */
+  active: PropTypes.bool,
+  className: PropTypes.string,
+  /**
+   * Mark the step as completed. Is passed to child components.
+   * @default false
+   */
+  completed: PropTypes.bool,
+  /**
+   * The label displayed in the step icon.
+   */
+  icon: PropTypes.node,
+};
+
+
+const step=10;
+export default function ProgressBar({step,steps}) {
+  return (
+    
+      
+      <Stepper alternativeLabel activeStep={(step-1)} connector={<ColorlibConnector/> }  >
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel StepIconComponent={ColorlibStepIcon}><Typography fontFamily='inter' fontWeight='600' fontSize='14px' >{label}</Typography></StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+  
+  );
+}
