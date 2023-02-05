4e5eaccf20ccc1570ac10db5c164388f5eb0327f
Yesitha Sathsara <60166952+yesitha@users.noreply.github.com>
2023-02-05 18:18:42 +0530

    Procurement Committee\View Item Page Added

---
+++ Code/frontend/src/components/StatusBulb/StatusBulb.js
@@ -0,0 +1,24 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+import styles from './StatusBulb.module.css'
+
+
+function setColur(status) {
+  if (status === 'Pending') {
+    return '#D29D04'
+  } else if (status === 'Approved') {
+    return '#227C70'
+  } else if (status === 'Rejected') {
+    return '#9C254D'
+  }
+}
+
+function StatusBulb({status}) {
+  return (
+    <div style={{backgroundColor:setColur(status)} }className={styles.StatusBulb}>
+      <Typography sx={{fontFamily:'inter' ,fontWeight:'600'}}>{status}</Typography>
+    </div>
+  )
+}
+
+export default StatusBulb
