08e1756216b625ff195d94be13ec0ff4fa66e751
Kalana Lakshan <kalanalakshang@gmail.com>
2023-06-15 03:22:07 +0530

    fix database, add ApprovedItemList controller

--- Code/backend/PWMSBackend/Models/SubProcurementApprovedItems.cs
+++ Code/backend/PWMSBackend/Models/SubProcurementApprovedItems.cs
@@ -6,8 +6,8 @@
         public string ItemId { get; set; }
         public SubProcurementPlan SubProcurementPlan { get; set; }
         public ApprovedItem ApprovedItem { get; set; }
-        public DateTime AuctionClosingDate { get; set; }
-        public DateTime AuctionOpeningDate { get; set; }
-        public DateTime PreBidMeetingDate { get; set; }
+        public DateTime? AuctionClosingDate { get; set; }
+        public DateTime? AuctionOpeningDate { get; set; }
+        public DateTime? PreBidMeetingDate { get; set; }
     }
 }
